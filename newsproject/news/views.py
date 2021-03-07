c51441b726421f7051c0e637bfd674b979543a70
shadhin <shahinur.rahman@evaly.com.bd>
2021-03-07 13:18:17 +0600

    set path for news

--- newsproject/news/views.py
+++ newsproject/news/views.py
@@ -3,17 +3,16 @@ from newsapi import NewsApiClient
 
 
 def index(request):
-
 	newsapi = NewsApiClient(api_key='cb8bca17789d40578a5c1b83184006f7')
 	top = newsapi.get_top_headlines(sources='techcrunch')
 
-	l = top['articles']
+	lm = top['articles']
 	desc = []
 	news = []
 	img = []
 
-	for i in range(len(l)):
-		f = l[i]
+	for i in range(len(lm)):
+		f = lm[i]
 		news.append(f['title'])
 		desc.append(f['description'])
 		img.append(f['urlToImage'])

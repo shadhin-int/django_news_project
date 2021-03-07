15292c81d51cf003ba60211d967133b9e5e37613
shadhin <shahinur.rahman@evaly.com.bd>
2021-03-07 12:37:31 +0600

    created views of news and add newsapi

--- newsproject/news/views.py
+++ newsproject/news/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from newsapi import NewsApiClient
 
-# Create your views here.
+
+def index(request):
+
+	newsapi = NewsApiClient(api_key='cb8bca17789d40578a5c1b83184006f7')
+	top = newsapi.get_top_headlines(sources='techcrunch')
+
+	l = top['articles']
+	desc = []
+	news = []
+	img = []
+
+	for i in range(len(l)):
+		f = l[i]
+		news.append(f['title'])
+		desc.append(f['description'])
+		img.append(f['urlToImage'])
+	mylist = zip(news, desc, img)
+
+	return render(request, 'index.html', context={"mylist": mylist})
